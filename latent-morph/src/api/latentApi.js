9c244725168df2c0974e507af31dba748b44d4e9
Manas Das <noragami0021@gmail.com>
2026-04-25 14:03:59 +0530

    fix: latent space api for autoencoder

--- latent-morph/src/api/latentApi.js
+++ latent-morph/src/api/latentApi.js
@@ -1,10 +1,10 @@
-export const decodeLatent = async (z) => {
+export const decodeLatent = async (z, model_name) => {
   const res = await fetch("http://127.0.0.1:8000/decode", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ z }),
+    body: JSON.stringify({ z, model_name: model_name }),
   });
 
   const data = await res.json();
